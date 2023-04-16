9630369b97cdfca84dcc1d772bbdbe5c25d2e208
dmitryfedyakin <d.fedyakin@g.nsu.ru>
2023-04-17 00:34:57 +0700

    dmitry_first_try

--- main.py
+++ main.py
@@ -41,7 +41,42 @@ def Ivan():
             total_tax += ans * 0.13 
 
 def Dmitry():
-    pass
+    #Obligation percents
+    ans = input(ru_local.R_OBL_PERCENT).lower()
+    if ans == ru_local.Y:
+        ans = input(ru_local.R_OBL_2007).lower()
+        if ans == ru_local.Y:
+            total_tax += ans * 0.09
+        else:
+            total_tax += ans * 0.13
+    else:
+        pass
+    
+    #Securities
+    ans = input(ru_local.R_SECURITIES).lower()
+    if ans == ru_local.Y:
+        total_tax += ans * 0.3
+    else:
+        pass
+
+    #Realty
+    ans = input(ru_local.R_REALTY).lower()
+    if ans == ru_local.Y:
+        total_tax += ans * 0.13
+    else:
+        pass
+
+    #Prize summary
+    ans_float = float(input(ru_local.R_PRIZE_SUMM).lower())
+    if ans_float >= 4000:
+        ans = input(ru_local.R_PRIZE_SUMM_4K).lower()
+        if ans == ru_local.Y:
+            total_tax += (ans_float - 4000) * 0.35
+        else:
+            total_tax += (ans_float - 4000) * 0.13
+
+    else:
+        pass
 
 def Daniil():
     pass
